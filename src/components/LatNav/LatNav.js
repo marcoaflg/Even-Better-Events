27b605c561193cc3b301d0717eb4e35cac27a4a1
Wilian Bueno <wilian.bueno.98@gmail.com>
2020-06-12 19:11:23 -0300

    Add LatNav

---
+++ src/components/LatNav/LatNav.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './App.css';
+import TouchAppIcon from '@material-ui/icons/TouchApp';
+import PersonIcon from '@material-ui/icons/Person';
+import EventAvailableIcon from '@material-ui/icons/EventAvailable';
+import CardMembershipIcon from '@material-ui/icons/CardMembership';
+import LanguageIcon from '@material-ui/icons/Language';
+import Arrow from '@material-ui/icons/ArrowForwardIos';
+
+
+
+const NavButton = ({onClick}) => 
+  <button className="nav-but" 
+   onClick={onClick}>
+      <Arrow fontSize="Small" />
+  </button>
+
+const Dropdown = ({show}) => 
+<div style={{visibility: show ? "visible" : "hidden",}}>
+
+<div className="nav-comp"> <TouchAppIcon fontSize="Large"/><br></br>Confirmar presença</div>
+      <div className="nav-comp"> <PersonIcon fontSize="Large"/><br></br>Perfil</div>
+      <div className="nav-comp"> <EventAvailableIcon fontSize="Large"/><br></br>Eventos</div>
+      <div className="nav-comp"> <CardMembershipIcon fontSize="Large"/><br></br>Certificados</div>
+      <div className="nav-comp"> <LanguageIcon fontSize="Large"/><br></br>Wilian Criador de Galaxias</div>
+     
+</div>
+
+
+
+
+export default class Parent extends React.Component {
+  state = {
+    dropdownVisible: true,
+  };
+
+  toggleDropdown = () => this.setState(state => ({
+    dropdownVisible: !state.dropdownVisible,
+  }));
+
+  
+
+  render() {
+    return (
+      <div className="LatNav">
+        <Dropdown show={this.state.dropdownVisible} />
+        <NavButton onClick={this.toggleDropdown} />
+    
+      </div>
+    );
+  }
+}
+
+
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './styles.css';
 import Header from '../../components/Header';
 import Slider from '../../components/Slider';
+import LatNav from '../../components/LatNav/LatNav';
 
 function Home() {
   return (
     <div className="Home">
       <Header />
       <Slider />
+      <LatNav />
     </div>
   );
 }
