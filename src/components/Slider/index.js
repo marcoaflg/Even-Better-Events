977357de023149f45dbaa4dda468c5c71b1f63c2
Marco Antônio Felipe de Lima Garcia <marcosl19@gmail.com>
2020-06-25 18:18:41 -0300

    Carousel settings changes

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,27 +1,43 @@
 import React, { Component } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import image1 from '../../assets/img/01.jpeg'
-import image2 from '../../assets/img/02.jpeg';
-import image3 from '../../assets/img/03.jpeg';
+import image1 from '../../assets/img/past.events/hackfest.jpeg'
+import image2 from '../../assets/img/past.events/gdgfoz.jpeg';
+import image3 from '../../assets/img/past.events/frontendday.png';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import './styles.css';
 
 
 class Slider extends Component {
-  render() {
-      return (
-          <Carousel autoPlay>
-              <div>
-                  <img alt="" src={image1}/>
-              </div>
-              <div>
-                  <img src={image2} />
-              </div>
-              <div>
-                  <img src={image3} />
-              </div>
-          </Carousel>
-      );
-  }
+    render() {
+        return (
+            <>
+                <CssBaseline />
+                <Container maxWidth="xl">
+                    <Carousel
+                        autoPlay
+                        infiniteLoop
+                        useKeyboardArrows
+                        emulateTouch
+                        dynamicHeight
+                        showThumbs={false}
+                        showStatus={false}
+                    >
+                        <div>
+                            <img alt="/" src={image1} />
+                        </div>
+                        <div>
+                            <img alt="/" src={image2} />
+                        </div>
+                        <div>
+                            <img alt="/" src={image3} />
+                        </div>
+                    </Carousel>
+                </Container>
+            </>
+        );
+    }
 };
 
 export default Slider;
