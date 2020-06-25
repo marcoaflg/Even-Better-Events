438a70deaead4492890e5ef65f96dea1a209dd46
jacikk <vinicius-jacik@hotmail.com>
2020-06-25 16:40:50 -0300

    Adding event presence confirmation modal, minor adaptations

--- src/components/LatNav/index.js
+++ src/components/LatNav/index.js
@@ -7,35 +7,25 @@ import CardMembershipIcon from '@material-ui/icons/CardMembership';
 import LanguageIcon from '@material-ui/icons/Language';
 import Arrow from '@material-ui/icons/ArrowForwardIos';
 import { Link } from 'react-router-dom';
+import EventAutentication from '../EventAutentication'
 
-
-
-
-
-const NavButton = ({onClick}) => 
-  <button className="nav-but" 
-   onClick={onClick}>
-      <Arrow fontSize="Small" />
+const NavButton = ({ onClick }) =>
+  <button className="nav-but"
+    onClick={onClick}>
+    <Arrow fontSize="Small" />
   </button>
 
-const Dropdown = ({show}) => 
-<div style={{visibility: show ? "hidden" : "visible"}}>
+const Dropdown = ({ show }) =>
+  <div style={{ visibility: show ? "hidden" : "visible" }}>
 
 
+    <div className="nav-comp"> <EventAutentication /></div>
+    <Link to="/Perfil" style={{ textDecoration: 'none', color: 'white' }}><div className="nav-comp"> <PersonIcon fontSize="Large" /><br></br>Perfil</div></ Link>
+    <Link to="/Event" style={{ textDecoration: 'none', color: 'white' }}><div className="nav-comp"> <EventAvailableIcon fontSize="Large" /><br></br>Eventos</div></ Link>
+    <Link to="/Certificates" style={{ textDecoration: 'none', color: 'white' }}><div className="nav-comp"> <CardMembershipIcon fontSize="Large" /><br></br>Certificados</div></Link>
+    <div className="nav-comp"> <LanguageIcon fontSize="Large" /><br></br>Wilian Criador de Galaxias</div>
 
-
-
-
-
-
-
-<div className="nav-comp" onClick={(e) => { if (window.confirm('Voce tem certeza que quer confirmar presença neste evento??')) this.deleteItem(e)}}> <TouchAppIcon fontSize="Large"/><br></br>Confirmar presença</div>
-      <Link to="/Perfil" style={{ textDecoration: 'none' , color: 'white'}}><div className="nav-comp"> <PersonIcon fontSize="Large"/><br></br>Perfil</div></ Link>
-      <Link to="/Event" style={{ textDecoration: 'none' , color: 'white'}}><div className="nav-comp"> <EventAvailableIcon fontSize="Large"/><br></br>Eventos</div></ Link>
-      <Link to="/Certificates" style={{ textDecoration: 'none', color: 'white' }}><div className="nav-comp"> <CardMembershipIcon fontSize="Large"/><br></br>Certificados</div></Link>
-      <div className="nav-comp"> <LanguageIcon fontSize="Large"/><br></br>Wilian Criador de Galaxias</div>
-     
-</div>
+  </div>
 
 
 
@@ -49,14 +39,14 @@ export default class Parent extends React.Component {
     dropdownVisible: !state.dropdownVisible,
   }));
 
-  
+
 
   render() {
     return (
-      <div className="LatNav" style={{ zIndex: this.state.dropdownVisible ? "0" : "1"}}>
+      <div className="LatNav" style={{ zIndex: this.state.dropdownVisible ? "0" : "1" }}>
         <Dropdown show={this.state.dropdownVisible} />
         <NavButton onClick={this.toggleDropdown} />
-    
+
       </div>
     );
   }
