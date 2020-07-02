97b3da15980abab1f2e9762a367142d8ab555bc1
Wilian Bueno <wilian.bueno.98@gmail.com>
2020-07-02 00:50:29 -0300

    Add GoToTop

--- src/pages/Event/index.js
+++ src/pages/Event/index.js
@@ -5,14 +5,17 @@ import LatNav from '../../components/LatNav';
 import BlocCards from '../../components/Cards/BlocCards';
 import Footer from '../../components/Footer'
 import SingleEvent from '../../components/SingleEvent'
+import GoTop from '../../components/GoTop'
 
 function Event() { 
 
     return (
         <div>
             <HeaderIn />
+            <GoTop />
             <SingleEvent />
             <LatNav />
+        
             <BlocCards />
             <Footer />
         </div>
